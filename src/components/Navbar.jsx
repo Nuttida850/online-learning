2b47e10b0593682d6eb077015be574260d3d1932
mirei <ovamirei@gmail.com>
2025-04-28 20:56:11 +0700

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,60 @@
-import React from 'react'
-import logo from '../assets/logos/logo-black.png'
+import React, { useState } from "react";
+import logo from "../assets/logos/logo-black.png";
+import { FaBars } from "react-icons/fa6";
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className='w-full'>
-        <div className='max-w-7xl p-4 flex flex-col md:flex-row md:justify-between items-center'>
-            <div>
-                <img src={logo} alt="" />
-            </div>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Sign Up</a></li>
-            </ul>
+    <div className="w-full">
+      <div className="max-w-7xl mx-auto p-4 md:px-0 flex flex-col md:flex-row md:justify-between md:items-center">
+        <div className="justify-start">
+          <img src={logo} alt="" />
         </div>
+        <ul
+          className={`${
+            !menuOpen ? "hidden" : "flex"
+          } items-center md:flex flex-col md:flex-row md:items-center md:space-x-8 text-[22px] space-y-5 md:space-y-0 mt-10 md:mt-0`}
+        >
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="inline-flex py-2 px-7 bg-white rounded-full text-[#6C6C6C]"
+            >
+              Login
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="inline-flex py-2 px-7 bg-[#F48C06] rounded-full text-white"
+            >
+              Sign Up
+            </a>
+          </li>
+        </ul>
+        <div className="md:hidden absolute right-10 top-10 text-3xl">
+          <FaBars onClick={toggleMenu} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

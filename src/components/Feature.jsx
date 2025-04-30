5bb60bf4aba2e841f0a98af1742704c63c33fba0
mirei <ovamirei@gmail.com>
2025-04-30 20:35:59 +0700

    more features

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import f01 from "../assets/features/f-1.png";
+import f02 from "../assets/features/f-2.png";
+import f03 from "../assets/features/f-3.png";
+import f04 from "../assets/features/f-4.png";
+import f05 from "../assets/features/f-5.png";
+
 import icon04 from "../assets/icons/icon-4.png";
 import icon05 from "../assets/icons/icon-5.png";
 import icon06 from "../assets/icons/icon-6.png";
 
 function Feature() {
   return (
-    <div className="py-10">
+    <div className="py-20">
       <div className="max-w-7xl mx-auto p-5 lg:p-0 text-center">
         <h2 className="text-4xl font-semibold text-[#2F327D] mb-10">
           Our <span className="text-[#F48C06]">Features</span>
@@ -15,40 +20,101 @@ function Feature() {
           This very extraordinary feature, can make learning activities more
           efficient
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-center py-20">
           <div>
             <img src={f01} alt="" />
           </div>
-          <div className="text-left">
-            <h3 className="text-4xl font-semibold text-[#2F327D]">
+          <div className="text-center md:text-left">
+            <h3 className="text-4xl font-semibold text-[#2F327D] leading-[1.5]">
               A <span className="text-[#F48C06]">user interface</span> designed
               for the classroom
             </h3>
-            <div className="flex items-center">
-              <span className="mr-5">
-                <img src={icon04} alt="" />
-              </span>
-              <p>
-                Teachers don’t get lost in the grid view and have a dedicated
-                Podium space.
-              </p>
-            </div>
-            <div className="flex">
-              <span>
-                <img src={icon05} alt="" />
-              </span>
-              <p>TA’s and presenters can be moved to the front of the class.</p>
-            </div>
-            <div className="flex">
-              <span>
-                <img src={icon06} alt="" />
-              </span>
-              <p>
-                Teachers can easily see all students and class data at one time.
-              </p>
-            </div>
+            <ul className="text-[#696984] text-xl space-y-5 mt-10">
+              <li className="flex items-center">
+                <span className="mr-10 p-3 rounded-full shadow-lg scale-[1.5]">
+                  <img src={icon04} alt="" />
+                </span>
+                <p>
+                  Teachers don’t get lost in the grid view and have a dedicated
+                  Podium space.
+                </p>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-10 p-3 rounded-full shadow-lg scale-[1.5]">
+                  <img src={icon05} alt="" />
+                </span>
+                <p>
+                  TA’s and presenters can be moved to the front of the class.
+                </p>
+              </li>
+              <li className="flex items-center">
+                <span className="mr-10 p-3 rounded-full shadow-lg scale-[1.5]">
+                  <img src={icon06} alt="" />
+                </span>
+                <p>
+                  Teachers can easily see all students and class data at one
+                  time.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="text-center md:text-left">
+            <h4 className="text-4xl text-[#2F327D] font-semibold leading-[1.5]">
+              <span className="text-[#F48C06]">Tools </span>
+              For Teachers And Learners
+            </h4>
+            <p className="text-xl text-[#696984] my-5">
+              Class has a dynamic set of teaching tools built to be deployed and
+              used during class. Teachers can handout assignments in real-time
+              for students to complete and submit.
+            </p>
+          </div>
+          <div>
+            <img src={f02} alt="" />
+          </div>
+          <div>
+            <img src={f03} alt="" />
+          </div>
+          <div className="text-center md:text-left">
+            <h4 className="text-4xl text-[#2F327D] font-semibold leading-[1.5]">
+              Assessments,
+              <br />
+              <span className="text-[#F48C06]">Quizzes </span> , Tests
+            </h4>
+            <p className="text-xl text-[#696984] my-5">
+              Easily launch live assignments, quizzes, and tests. Student
+              results are automatically entered in the online gradebook.
+            </p>
+          </div>
+          <div className="text-center md:text-left">
+            <h4 className="text-4xl text-[#2F327D] font-semibold leading-[1.5]">
+              <span className="text-[#F48C06]">Class Management </span>
+              Tools for Educators{" "}
+            </h4>
+            <p className="text-xl text-[#696984] my-5">
+              Class provides tools to help run and manage the class such as
+              Class Roster, Attendance, and more. With the Gradebook, teachers
+              can review and grade tests and quizzes in real-time.
+            </p>
+          </div>
+          <div>
+            <img src={f04} alt="" />
+          </div>
+          <div>
+            <img src={f05} alt="" />
+          </div>
+          <div className="text-center md:text-left">
+            <h4 className="text-4xl text-[#2F327D] font-semibold leading-[1.5]">
+              One-on-One <br />
+              <span className="text-[#F48C06]">Discussions </span>
+            </h4>
+            <p className="text-xl text-[#696984] my-5">
+              Teachers and teacher assistants can talk with students privately
+              without leaving the Zoom environment.
+            </p>
           </div>
         </div>
+        <a href="#" className="px-6 py-5 rounded-full border-1 border-[#F48C06] text-[#F48C06] text-xl">See more features</a>
       </div>
     </div>
   );
